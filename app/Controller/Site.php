705e569540e8f42b64b09e5798d03911154d7522
MblWb-CoNR <sonia.4545456@gmail.com>
2025-03-26 16:25:20 +0700

    authentication method

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -6,6 +6,7 @@ use Model\Post;
 use Src\View;
 use Src\Request;
 use Model\User;
+use Src\Auth\Auth;
 
 class Site
 {
@@ -14,7 +15,6 @@ class Site
         $posts = Post::where('id', $request->id)->get();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
-
     public function hello(): string
     {
         return (new View())->render('site.hello', ['message' => 'hello working']);
@@ -25,6 +25,24 @@ class Site
             app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
 
