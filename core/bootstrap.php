7562f83de21aabbb8a5bf06676c21f686ed34682
MblWb-CoNR <sonia.4545456@gmail.com>
2025-03-26 17:16:01 +0700

    add and setting user's role

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -18,7 +18,7 @@ function getConfigs(string $path = DIR_CONFIG): array
     return $settings;
 }
 
-require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../route/web.php';
 $app = new Src\Application(new Src\Settings(getConfigs()));
 
 //Функция возвращает глобальный экземпляр приложения
