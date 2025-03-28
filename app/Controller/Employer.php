002bea09325955873bcf5c0b2980a757d05df2d7
MblWb-CoNR <sonia.4545456@gmail.com>
2025-03-28 16:16:55 +0700

    beginning of the layout

---
+++ app/Controller/Employer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Controller;
+
+use Model\Post;
+use Src\View;
+use Src\Request;
+use Model\User;
+use Src\Auth\Auth;
+class Employer
+{
+
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,10 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../public/css/main.css">
     <title>Pop it MVC</title>
 </head>
 <body>
 <header>
+    <img src="../../public/img/logo.svg" alt="uni">
     <nav>
         <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
         <?php
@@ -27,6 +29,7 @@
 </header>
 <main>
     <?= $content ?? '' ?>
+
 </main>
 
 </body>
